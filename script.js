d1006f288331d4a6f11a6433ccad1f986b60846f
jmtopacio <jmtopacio27@gmail.com>
2026-06-04 15:37:23 +0800

    Update portfolio scripts and theme fixes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "I LOVE YOU SO MUCH BABY ANYA KO ";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
